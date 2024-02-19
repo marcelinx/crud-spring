5ebe9557276aa3e3a88b6b05d0578c68a39a7abb
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-02-18 23:31:50 -0300

    feat: Validações da API com Java Bean Validation

--- src/main/java/com/marcelinx/crudspring/controller/CourseController.java
+++ src/main/java/com/marcelinx/crudspring/controller/CourseController.java
@@ -1,72 +1,62 @@
 package com.marcelinx.crudspring.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.marcelinx.crudspring.model.Course;
 import com.marcelinx.crudspring.repository.CourseRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
 
-  private final CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
-  @GetMapping
-  public @ResponseBody List<Course> list() {
-    return courseRepository.findAll();
-  }
+    @GetMapping
+    public @ResponseBody List<Course> list() {
+        return courseRepository.findAll();
+    }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<Course> findById(@PathVariable Long id) {
-    return courseRepository.findById(id)
-        .map(recordFound -> ResponseEntity.ok().body(recordFound))
-        .orElse(ResponseEntity.notFound().build());
-  }
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-  @PostMapping
-  @ResponseStatus(code = HttpStatus.CREATED)
-  public Course create(@RequestBody Course course) {
-    return courseRepository.save(course);
-  }
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody @Valid Course course) {
+        return courseRepository.save(course);
+    }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
-    return courseRepository.findById(id)
-        .map(recordFound -> {
-          recordFound.setName(course.getName());
-          recordFound.setCategory(course.getCategory());
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-          Course updated = courseRepository.save(recordFound);
-
-          return ResponseEntity.ok().body(updated);
-
-        })
-        .orElse(ResponseEntity.notFound().build());
-  }
-
-  @DeleteMapping("/{id}")
-  public ResponseEntity<?> delete(@PathVariable Long id) {
-    return courseRepository.findById(id)
-        .map(record -> {
-          courseRepository.deleteById(id);
-          return ResponseEntity.noContent().<Void>build(); 
-        })
-        .orElse(ResponseEntity.notFound().build());
-  }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+                .map(record -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/marcelinx/crudspring/model/Course.java
+++ src/main/java/com/marcelinx/crudspring/model/Course.java
@@ -1,6 +1,5 @@
 package com.marcelinx.crudspring.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -8,6 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -19,9 +22,16 @@ public class Course {
   @JsonProperty("_id")
   private Long id;
 
-  @Column(length = 200, nullable = false)
+  @NotBlank
+  @NotNull
+  @org.hibernate.validator.constraints.Length(min = 5, max = 100)
+  @Column(length = 100, nullable = false)
   private String name;
 
-  @Column(length = 12, nullable = false)
+  @NotNull
+  @org.hibernate.validator.constraints.Length(max = 10)
+  @Pattern(regexp = "Back-end|Front-end")
+  @Column(length = 10, nullable = false)
   private String category;
+
 }
